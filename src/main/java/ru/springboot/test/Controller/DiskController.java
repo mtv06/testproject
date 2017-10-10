26982a4a43c1dcced321e39ff5b71de8331d8db7
amotov <motov.1984@mail.ru>
2017-10-10 18:39:19 +0300

    commit

--- src/main/java/ru/springboot/test/Controller/DiskController.java
+++ src/main/java/ru/springboot/test/Controller/DiskController.java
@@ -1,13 +1,19 @@
 package ru.springboot.test.Controller;
 
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import ru.springboot.test.Model.Disk;
 import ru.springboot.test.Service.DiskServiceImpl;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Controller
-@RequestMapping
 public class DiskController {
 
     @Autowired
@@ -19,8 +25,12 @@ public class DiskController {
 //        return new ResponseEntity<List<Disk>>(list, HttpStatus.OK);
 //    }
 
-    @GetMapping("/")
+    @RequestMapping(value="/", method = RequestMethod.GET)
     public String allDisk(){
-        return diskService.getAllDisk().toString();
+        Disk user = new Disk(); //можем здесь, например, достать из базы пользователя с ником name
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+
+        return user;
     }
 }
